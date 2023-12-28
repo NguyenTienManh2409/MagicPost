8b030c7c7e5043fb867649e7191b9765b140575e
Maheu1010 <100405724+Maheu1010@users.noreply.github.com>
2023-12-28 17:37:45 +0700

    Update login.php

--- backend/api/login.php
+++ backend/api/login.php
@@ -2,7 +2,7 @@
 
     header('Access-Control-Allow-Origin:*');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+
 
     include_once('../config/db.php');
     include_once('../model/account.php');
